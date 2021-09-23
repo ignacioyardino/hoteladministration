c93b1e84fb39cec2cd254fff73ff89a7b87aa7f6
Ignacio Yardino Rodríguez <88339882+ignacioyardino@users.noreply.github.com>
2021-09-23 01:24:47 -0300

    uepale

--- js/nav.js
+++ js/nav.js
@@ -7,9 +7,18 @@ document.addEventListener("DOMContentLoaded", function (e) {
         <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
         <a class="py-2 d-none d-md-inline-block" href="products.html">Productos</a>
         <a class="py-2 d-none d-md-inline-block" href="sell.html">Vender</a>
-        <a class="py-2 d-none d-md-inline-block" href="cart.html">Mi carrito</a>
-        <a class="py-2 d-none d-md-inline-block" href="my-profile.html">` + localStorage.getItem("USUARIO") + `</a>
-        <a class="py-2 d-none d-md-inline-block" id="cerrar_btn" href="#">Cerrar Sesión</a>
+        <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  ` + localStorage.getItem("USUARIO") + `
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+  <a class="dropdown-item" href="cart.html" type="button">Mi carrito</a>
+  <a class="dropdown-item" href="my-profile.html" type="button">Mi perfil</a>
+  <a class="dropdown-item" id="cerrar_btn" type="button" href="#">Cerrar Sesión</a>
+  </div>
+</div>
+       
+      
         </div>
     </nav>`
   document.getElementById("contenedordelnav").innerHTML = barra;
