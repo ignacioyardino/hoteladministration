1e54de68b38aedca2a4ddb82c9e75ebad50c91d1
Ignacio Yardino Rodríguez <88339882+ignacioyardino@users.noreply.github.com>
2021-10-01 16:03:39 -0300

    NV

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,18 @@
 
 document.addEventListener("DOMContentLoaded", function (e) {
   var barra =
-    `<nav class="site-header sticky-top py-1 bg-dark">
+    ` <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container d-flex flex-column flex-md-row justify-content-between">
         <a class="py-2 d-none d-md-inline-block" href="home.html">Inicio</a>
         <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
         <a class="py-2 d-none d-md-inline-block" href="products.html">Productos</a>
         <a class="py-2 d-none d-md-inline-block" href="sell.html">Vender</a>
+        <a class="py-2 d-none d-md-inline-block"  href="cart.html">Mi carrito</a>
         <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  ` + localStorage.getItem("USUARIO") + `
+  <button class="btn btn bg-light text-primary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+   ` + localStorage.getItem("USUARIO") + `
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-  <a class="dropdown-item" href="cart.html" type="button">Mi carrito</a>
   <a class="dropdown-item" href="my-profile.html" type="button">Mi perfil</a>
   <a class="dropdown-item" id="cerrar_btn" type="button" href="#">Cerrar Sesión</a>
   </div>
