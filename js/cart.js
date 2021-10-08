ef7af1b88942fe6e61fd868c6e1b5a7783a7f8f9
Ignacio Yardino Rodríguez <88339882+ignacioyardino@users.noreply.github.com>
2021-10-08 16:13:13 -0300

    a

--- js/cart.js
+++ js/cart.js
@@ -1,53 +1,100 @@
-var cartData = []
+var carrito = {};
 
-
-function convertirMoneda(moneda, precio) {
-  if (monedaActual == "UYU") {
-
-      if (monedaActual == moneda + precio) {
-        return precio
-      }
-    } else {
-      return precio * 40
-    }
-  }
 
 document.addEventListener("DOMContentLoaded", function (e) {
-  getJSONData(CART_INFO_URL).then(function (resultObj) {
-    if (resultObj.status === "ok") {
-      cartData = resultObj.data.articles;
-      showCart();
-    }
-  });
+  dibujoVariables()
+  dibujoCarrito();
+
 });
 
+function dibujoVariables() {
+  getJSONData(CART_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      carrito = resultObj.data.articles;
 
-function showCart() {
-
-  var contenedor = document.getElementById("cart-container")
-
-  for (var i = 0; i < cartData.length; i++) {
-    const carItem = cartData[i];
-    var itemPrecio = convertirMoneda(cartItem.currency, cartItem.unitCost)
-    contenedor.innerHTML += `
-            <div class="row p-2 cart-item">
-            <div class="col-5"><img src="${cartItem.src}" class="img-fluid"></div>
-            <div class="col-5"><h6 class "mt-5">${monedaActual + itemPrecio} </h5></div>
-            <div class ="col-1 m-0">
-            <input class="mt-5" type="number" name=$(i) min="1" max="99" onchange="updateCart(item.value, this.name)" value="${cartItem.articlesCount}>
-            </div>
-            <div class="col-2"><h5 class="text-center mt-5 subtotal">${monedaActual + itemPrecio * cartItem.articlesCount}</h5></div>
-            </div>`
-
-    document.getElementById("carrito").innerHTML = htmlContentToAppend;
-  }
+      let articlesNameHTML = document.getElementById("articlesName");
+      let articlesCountHTML = document.getElementById("articlesCount");
+      let articlesUnitCostHTML = document.getElementById("articles.unitCost");
+      let articlesCurrencyHTML = document.getElementById("articles.currency");
+      let articlesSrcHTML = document.getElementById("articles.src");
+      //
+      articlesNameHTML.innerHTML = articles.name;
+      articlesCountHTML.innerHTML = articlesCount;
+      articlesUnitCostHTML.innerHTML = articles.unitCost;
+      articlesCurrencyHTML.innerHTML = articles.currency;
+      articlesSrcHTML.innerHTML = articles.src;
+    }
+  });
 }
 
+function dibujoCarrito() {
+  getJSONData(CART_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      carrito = resultObj.data;
+      let htmlContentToAppend = "";
+      for (let i = 0; i < carrito.length; i++) {
+        var carrito = dibujoCarrito(articles[i]);
+        htmlContentToAppend += `
+    <section>
+    <!--Grid row-->
+    <div class="row">
 
-function updateCart(valor, id) {
-  var items = document.getElementsByClassName("cart-item")
-  var itemSelected = itemds[id]
-  var subtotal = valor * cartData[id].unitCost
-  itemSelected.getElementsByClassName("subtotal")[0].innerHTML = cartData[id].currency + subtotal
+      <!--Grid column-->
+      <div class="col-lg-8">
 
+        <!-- Card -->
+        <div class="mb-3">
+          <div class="pt-4 wish-list">
+
+            <h5 class="mb-4">Cart (<span>2</span> items)</h5>
+
+            <div class="row mb-4">
+              <div class="col-md-5 col-lg-3 col-xl-3">
+                <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
+                  <img class="img-fluid w-100"
+                  <img src="`+ "src": "img/tree1.jpg" + `" alt="Los Angeles">
+                  <a href="#!">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-7 col-lg-9 col-xl-9">
+                <div>
+                  <div class="d-flex justify-content-between">
+                    <div>
+                    <h4>`+ articles[i].name + `</h4>
+                      <p class="mb-3 text-muted text-uppercase small">Shirt - blue</p>
+                      <p class="mb-2 text-muted text-uppercase small">Stock: `+ articles[i].count + `</p>                      
+                      <p class="mb-3 text-muted text-uppercase small">Costo:`+ articles[i].unitCost + `: M</p>
+                    </div>
+                    <div>
+                      <div class="def-number-input number-input safari_only mb-0 w-100">
+                        <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+                          class="minus decrease"></button>
+                        <input class="quantity" min="0" name="quantity" value="1" type="number">
+                        <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+                          class="plus increase"></button>
+                      </div>
+                      <small id="passwordHelpBlock" class="form-text text-muted text-center">
+                        (Note, 1 piece)
+                      </small>
+                    </div>
+                  </div>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                      <a href="#!" type="button" class="card-link-secondary small text-uppercase mr-3"><i
+                          class="fas fa-trash-alt mr-1"></i> Remove item </a>
+                      <a href="#!" type="button" class="card-link-secondary small text-uppercase"><i
+                          class="fas fa-heart mr-1"></i> Move to wish list </a>
+                    </div>
+                    <p class="mb-0"><span><strong id="summary">$17.99</strong></span></p class="mb-0">
+                  </div>
+                </div>
+              </div>
+            </div>
+            </section>   
+            `
+      }
+      document.getElementById("carrito").innerHTML = htmlContentToAppend;
+    }
+  })
 }
